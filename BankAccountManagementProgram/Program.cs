fba4d02d5bda7fdd3cb54da0e3825eff12a9b40f
Athame0802 <266697209+Athame0802@users.noreply.github.com>
2026-07-25 17:35:56 +0900

    feat: 프로젝트 생성
    - C# 솔루션 파일 생성

---
+++ BankAccountManagementProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BankAccountManagementProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
